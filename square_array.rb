7ae14a865165f22596c53fd8e2620cc122c17b3a
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-10-23 17:08:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,7 @@
 def square_array(numbers)
   numbers = [1,2,3]
-  square_array(numbers).each {|numbers| i ** 2}
-end
-
-
-def square_array(numbers)
-  numbers = [1,2,3]
   numbers.each do |num|
-  new_numbers << (num ** 2)
+    new_numbers << (num **2)
   end
   return new_numbers
 end
